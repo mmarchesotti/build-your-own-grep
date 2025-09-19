c7a4ebfb7cf30541fc0b8499314e888c500f81df
Matheus Ferraz <matheus.ferraz@treinus.com.br>
2025-09-18 22:41:53 -0300

    Updating lexer to match pattern

--- app/lexer/lexer.go
+++ app/lexer/lexer.go
@@ -1,4 +1,4 @@
-package main
+package lexer
 
 import (
 	"strings"
@@ -9,72 +9,73 @@ import (
 func Parse(inputPattern string) []pattern.Pattern {
 	var patterns []pattern.Pattern
 
-	for i := 0; i < len(inputPattern); i++ {
-		currentCharacter := inputPattern[i]
+	for inputIndex := 0; inputIndex < len(inputPattern); inputIndex++ {
+		currentCharacter := inputPattern[inputIndex]
 
 		switch currentCharacter {
 		case '\\':
-			nextCharacter := inputPattern[i+1]
-			switch nextCharacter {
-			case 'd':
-				patterns = append(patterns, &pattern.Digit{})
-				i += 1
-			case 'w':
-				patterns = append(patterns, &pattern.AlphaNumeric{})
-				i += 1
-			default:
-				patterns = append(patterns, &pattern.Literal{Literal: "\\"})
+			if inputIndex+1 < len(inputPattern) {
+				nextCharacter := inputPattern[inputIndex+1]
+				switch nextCharacter {
+				case 'd':
+					patterns = append(patterns, &pattern.Digit{})
+					inputIndex += 1
+				case 'w':
+					patterns = append(patterns, &pattern.AlphaNumeric{})
+					inputIndex += 1
+				default:
+					patterns = append(patterns, &pattern.Literal{Literal: '\\'})
+				}
+			} else {
+				patterns = append(patterns, &pattern.Literal{Literal: '\\'})
 			}
 		case '[':
-			closingIndex := strings.Index(inputPattern[i+1:], "]")
+			closingIndex := strings.Index(inputPattern[inputIndex+1:], "]")
 			if closingIndex == -1 {
-				patterns = append(patterns, &pattern.Literal{Literal: "["})
+				patterns = append(patterns, &pattern.Literal{Literal: '['})
 				continue
 			}
 
-			matches := 
-			nextCharacter := inputPattern[i+1]
-			
-		default:
-			patterns = append(patterns, &pattern.Literal{Literal: inputPattern[i : i+1]})
-		}
-	}
+			var groupPatterns []pattern.Pattern
+			groupCharacters := inputPattern[inputIndex+1 : closingIndex]
+			for groupIndex := 0; groupIndex < len(groupCharacters); groupIndex++ {
+				currentGroupCharacter := groupCharacters[groupIndex]
 
-	return patterns
-}
-
-
-func Parse(inputPattern string) []pattern.Pattern {
-	var patterns []pattern.Pattern
-
-	for i := 0; i < len(inputPattern); i++ {
-		currentCharacter := inputPattern[i]
-
-		switch currentCharacter {
-		case '\\':
-			nextCharacter := inputPattern[i+1]
-			switch nextCharacter {
-			case 'd':
-				patterns = append(patterns, &pattern.Digit{})
-				i += 1
-			case 'w':
-				patterns = append(patterns, &pattern.AlphaNumeric{})
-				i += 1
-			default:
-				patterns = append(patterns, &pattern.Literal{Literal: "\\"})
-			}
-		case '[':
-			closingIndex := strings.Index(inputPattern[i+1:], "]")
-			if closingIndex == -1 {
-				patterns = append(patterns, &pattern.Literal{Literal: "["})
-				continue
+				if currentGroupCharacter == '\\' && groupIndex+1 < len(inputPattern) {
+					nextCharacter := inputPattern[inputIndex+1]
+					switch nextCharacter {
+					case 'd':
+						groupPatterns = append(groupPatterns, &pattern.Digit{})
+						groupIndex += 1
+					case 'w':
+						groupPatterns = append(groupPatterns, &pattern.AlphaNumeric{})
+						groupIndex += 1
+					default:
+						groupPatterns = append(groupPatterns, &pattern.Literal{Literal: '\\'})
+					}
+				} else {
+					groupPatterns = append(groupPatterns, &pattern.Literal{
+						Literal: rune(groupCharacters[groupIndex]),
+					})
+				}
 			}
 
-			matches := 
-			nextCharacter := inputPattern[i+1]
-			
+			groupFirstCharacter := groupCharacters[0]
+			if groupFirstCharacter == '^' {
+				patterns = append(patterns, &pattern.NegativeGroup{Patterns: groupPatterns[1:]})
+			} else {
+				patterns = append(patterns, &pattern.PositiveGroup{Patterns: groupPatterns})
+			}
+
+			inputIndex += closingIndex + 1
+		case '^':
+			patterns = append(patterns, &pattern.EndAnchor{})
+		case '$':
+			patterns = append(patterns, &pattern.StartAnchor{})
 		default:
-			patterns = append(patterns, &pattern.Literal{Literal: inputPattern[i : i+1]})
+			patterns = append(patterns, &pattern.Literal{
+				Literal: rune(inputPattern[inputIndex]),
+			})
 		}
 	}
 
